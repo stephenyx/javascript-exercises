37a52c72045aba97c7806a63a719e67ab770891f
stephenyx <stephen.xia@gmail.com>
2022-06-22 19:00:06 -0400

    Use for-of loop to test for index and splice original array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,13 @@
 const removeFromArray = function(arr, ...args) {
-    for (let arg of args) {
-        let idx = arr.indexOf(arg);
-        arr.splice(idx,1);  
-    }
-    return arr;
+    // for (let arg of args) {
+    //     let idx = arr.indexOf(arg);
+    //     if (idx != -1) {
+    //         arr.splice(idx,1);
+    //     }
+    // }
+    // return arr;
+
+    return arr.filter(val =>!args.includes(val));
 };
 
 // Do not edit below this line
