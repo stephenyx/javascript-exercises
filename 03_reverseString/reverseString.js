970c383d035785c1e61ba50b25f98bc09ce98c05
stephenyx <stephen.xia@gmail.com>
2022-06-22 14:08:29 -0400

    Complete assignment using for and while loops

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,18 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let temp = string.split("");
+    let reverseHolder = [];
 
+    // for (let i = string.length-1; i >= 0; i--) {
+    //     reverseHolder.push(temp.pop());
+    // }
+
+    let tempLen = string.length;
+    while (tempLen) {
+        reverseHolder.push(temp.pop());
+        tempLen--;
+    }
+
+    return reverseHolder.join('');
 };
 
 // Do not edit below this line
