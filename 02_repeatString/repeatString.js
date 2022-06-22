e902aed0faaeaabcc5680fe9ac9f1ec9dfeca470
stephenyx <stephen.xia@gmail.com>
2022-06-22 13:46:14 -0400

    Complete repeatString function and add error msg

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let tempString = '';
+    if (num < 0) {
+        return "ERROR";
+    } else {
+        for (i = 0; i < num; i++) {
+            tempString += `${string}`;    
+    }
+    }
+    return tempString;
 };
 
 // Do not edit below this line
