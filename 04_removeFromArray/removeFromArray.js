ff898a4f51935c7c382cba6fda9a81a8591076e3
stephenyx <stephen.xia@gmail.com>
2022-06-22 14:49:52 -0400

    Pass second test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,8 @@
-const removeFromArray = function(arr, arg) {
-    let idx = arr.indexOf(arg);
-    arr.splice(idx,1);
+const removeFromArray = function(arr, ...args) {
+    for (let arg of args) {
+        let idx = arr.indexOf(arg);
+        arr.splice(idx,1);  
+    }
     return arr;
 };
 
